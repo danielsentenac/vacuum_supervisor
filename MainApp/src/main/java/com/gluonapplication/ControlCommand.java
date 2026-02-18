cbc6f3bead4a4f4c907f0982b6c226919bfce38c
daniel.sentenac <daniel.sentenac@ligo.org>
2026-02-18 14:57:22 +0100

    Release v0.2.1

--- MainApp/src/main/java/com/gluonapplication/ControlCommand.java
+++ MainApp/src/main/java/com/gluonapplication/ControlCommand.java
@@ -76,7 +76,7 @@ public class ControlCommand implements Runnable {
          hasMastership = false;            
          hasPrivilegeAccess = false;
          // send query to the servlet
-         URLConnection con = getServerConnection("http://olserver135.virgo.infn.it:8082/jmaster/jmaster");
+         URLConnection con = getServerConnection("http://olserver135.virgo.infn.it:8081/jmaster/jmaster");
          OutputStream outstream = con.getOutputStream();
          ObjectOutputStream oos = new ObjectOutputStream(outstream);
          oos.writeObject(query);
--- MainApp/src/main/java/com/gluonapplication/SidePopupViewData.java
+++ MainApp/src/main/java/com/gluonapplication/SidePopupViewData.java
@@ -89,7 +89,7 @@ public class SidePopupViewData implements Runnable, DataTypes {
       try {
          // send data to the servlet
          DataSet tmpData = (DataSet) data.clone();
-         URLConnection con = getServerConnection("http://olserver135.virgo.infn.it:8082/jchv/jchv");
+         URLConnection con = getServerConnection("http://olserver135.virgo.infn.it:8081/jchv/jchv");
          OutputStream outstream = con.getOutputStream();
          ObjectOutputStream oos = new ObjectOutputStream(outstream);
          oos.writeObject(data.svrNameList);
